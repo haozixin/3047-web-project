8c2901a4e0202ccf79172f70784f8580deded7d4
Adrian1027 <cche0104@student.monash.edu>
2021-09-06 20:51:45 +0800

    ALL FAQ -> Q&A

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -198,7 +198,7 @@ $cakeDescription = 'CakePHP: the rapid development PHP framework';
                     <a class="collapse-item" href="login.html">Login</a>
                     <a class="collapse-item" href="register.html">Register</a>
                     <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                    <a class="collapse-item" href="FAQ.html">FAQ</a>
+                    <a class="collapse-item" href="<?= $this->Url->build(['controller'=>'Customers','action'=>'faq']) ?>">Q&A</a>
                     <div class="collapse-divider"></div>
                     <h6 class="collapse-header">Other Pages:</h6>
                     <a class="collapse-item" href="404.html">404 Page</a>
