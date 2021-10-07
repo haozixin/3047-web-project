f624cd208e1156bdcc308b2c7510b88eef5a590a
Zixin Hao <zhao0004@student.monash.edu>
2021-10-07 15:16:42 +0800

    fix bugs for nav

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -63,7 +63,7 @@ class AppController extends Controller
         parent::beforeFilter($event);
         // for all controllers in our application, make index and view
         // actions public, skipping the authentication check
-        $this->Authentication->addUnauthenticatedActions(['controller' => 'Users', 'action' => 'add', 'display','addforcustomer']);
+        $this->Authentication->addUnauthenticatedActions(['controller' => 'Users', 'action' => 'add', 'display','addforcustomer','displaycustomer','faq']);
 
     }
 
