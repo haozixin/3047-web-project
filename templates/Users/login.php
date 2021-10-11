fea4f94a8f759012f1ad7ec42a49c67c47bb06ca
Zixin Hao <zhao0004@student.monash.edu>
2021-10-11 10:36:39 +0800

    simple button on login page

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -89,7 +89,7 @@ $this->Form->setTemplates($formTemplate);
                                 <hr>
                                 </fieldset>
                                 <div class="text-center">
-                                    <?= $this->Html->link("Sign up for backend", ['controller' => 'Users', 'action' => 'add']) ?>
+                                    <button><?= $this->Html->link("Sign up for backend", ['controller' => 'Users', 'action' => 'add']) ?></button>
                                     <a class="small" href=""
                                        Create an Account!</a>
                                 </div>
