8c2d85d453c422f814fb8a703c83818f9ba884a7
Adrian1027 <cche0104@student.monash.edu>
2021-10-13 01:51:54 +0800

    IT 2 Huge fix from checklist in trello

--- templates/NewsletterSubscriptions/edit.php
+++ templates/NewsletterSubscriptions/edit.php
@@ -3,19 +3,19 @@
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\NewsletterSubscription $newsletterSubscription
  */
+ $formTemplate = [
+     'input' => '<input type="{{type}}" name="{{name}}" class="form-control" {{attrs}}/>',
+     'inputSubmit' => '<input type="{{type}}"{{attrs}}/>',
+     'inputContainer' => '<div class="input {{type}}{{required}}">{{content}}</div>',
+     'inputContainerError' => '<div class="input {{type}}{{required}} error">{{content}}{{error}}</div>',
+     'label' => '<label{{attrs}}class="form-label">{{text}}</label>',
+     'textarea' => '<textarea name="{{name}}"class="form-control"{{attrs}}>{{value}}</textarea>',
+
+ ];
+ $this->Form->setTemplates($formTemplate);
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Form->postLink(
-                __('Delete'),
-                ['action' => 'delete', $newsletterSubscription->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $newsletterSubscription->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Newsletter Subscriptions'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
+
+
     <div class="column-responsive column-80">
         <div class="newsletterSubscriptions form content">
             <?= $this->Form->create($newsletterSubscription) ?>
@@ -26,8 +26,9 @@
                 echo $this->Form->control('customer_email');
                 ?>
             </fieldset>
+             <br></br>
             <?= $this->Form->button(__('Submit')) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
-</div>
+
